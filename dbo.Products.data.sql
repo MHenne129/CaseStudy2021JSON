e61506477e57ced87b13120c9ee244ee8ec0b635
MHenne129 <43079590+MHenne129@users.noreply.github.com>
2021-07-09 18:37:36 -0400

    Update dbo.Products.data.sql

--- dbo.Products.data.sql
+++ dbo.Products.data.sql
@@ -8,7 +8,7 @@
       "FBR":"50",
       "ITEM":"he next generation of Xbox gaming comes now in the most power console in the world, Xbox Series X",
       "PRO":"40",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Microsoft"
    },
    {
       "CAL":"Xbox Series S",
@@ -19,7 +19,7 @@
       "FBR":"15",
       "ITEM":"Experience next generation gaming at an afforable price on the Xbox Series S",
       "PRO":"45",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Microsoft"
    },
    {
       "CAL":"Halo Infinite",
@@ -30,7 +30,7 @@
       "FBR":"150",
       "ITEM":"Halo Infinite is an upcoming first-person shooter game developed by 343 Industries and published by Xbox Game Studios for Windows, Xbox One, and Xbox Series X and Series S. The sixth main entry of the Halo series, and the sixteenth Halo game overall, it picks up the story of the Master Chief, following Guardians",
       "PRO":"40",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Microsoft"
    },
    {
       "CAL":"Forza Horizon 5",
@@ -41,7 +41,7 @@
       "FBR":"100",
       "ITEM":"Forza Horizon 5 is an upcoming racing game developed by Playground Games and published by Xbox Game Studios. It will be the fifth Forza Horizon title and twelfth main instalment in the Forza series. The game is set in a fictionalised representation of Mexico",
       "PRO":"14",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Microsoft"
    },
    {
       "CAL":"Fable",
@@ -52,7 +52,7 @@
       "FBR":"80",
       "ITEM":"Fable returns in the reboot of the classic Xbox franchise coming exclusively to Xbox and PC",
       "PRO":"14",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Microsoft"
    },
    {
       "CAL":"PS5",
@@ -63,7 +63,7 @@
       "FBR":"20",
       "ITEM":"The next generation of Sony PlayStatio is here on the PS5",
       "PRO":"20",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Sony"
    },
    {
       "CAL":"PS5 Digital",
@@ -74,7 +74,7 @@
       "FBR":"22",
       "ITEM":"Play next gen PS5 games at a lower cost with the digital only PS5 console",
       "PRO":"22",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Sony"
    },
    {
       "CAL":"God of War Ragnarok",
@@ -85,7 +85,7 @@
       "FBR":"100",
       "ITEM":"The untitled God of War sequel is an action-adventure game currently in development by Santa Monica Studio and will be published by Sony Interactive Entertainment. It is scheduled to be released in 2022 for the PlayStation 4 and PlayStat0ion 5",
       "PRO":"25",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Sony"
    },
    {
       "CAL":"Horizon Forbidden West",
@@ -96,7 +96,7 @@
       "FBR":"80",
       "ITEM":"Horizon Forbidden West is an upcoming action role-playing game developed by Guerrilla Games and published by Sony Interactive Entertainment. It is set to be released in 2021 for the PlayStation 4 and PlayStation 5. A sequel to Horizon Zero Dawn, it features an open world in a post-apocalyptic western United States",
       "PRO":"15",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Sony"
    },
    {
       "CAL":"Ratchet & Clank: Rift Apart",
@@ -107,7 +107,7 @@
       "FBR":"40",
       "ITEM":"Ratchet & Clank: Rift Apart is a 2021 third-person shooter platform game developed by Insomniac Games and published by Sony Interactive Entertainment for the PlayStation 5. It is the sixteenth installment in the Ratchet & Clank series. The game takes place following the events of Ratchet & Clank: Into the Nexus.",
       "PRO":"22",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Sony"
    },
    {
       "CAL":"Nintendo Switch",
@@ -118,7 +118,7 @@
       "FBR":"60",
       "ITEM":"The Nintendo Switch is a hybrid video game console, consisting of a console unit, a dock, and two Joy-Con controllers. Although it is a hybrid console, Nintendo classifies it as 'a home console that you can take with you on the go'",
       "PRO":"36",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Nintendo"
    },
    {
       "CAL":"Nintendo Switch Lite",
@@ -129,7 +129,7 @@
       "FBR":"15",
       "ITEM":"The Nintendo Switch Lite is designed specifically for handheld play so you can jump into your favorite games wherever you happen to be.",
       "PRO":"41",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Nintendo"
    },
    {
       "CAL":"Zelda Breath of the Wild 2",
@@ -140,7 +140,7 @@
       "FBR":"100",
       "ITEM":"An untitled sequel to the 2017 action-adventure game The Legend of Zelda: Breath of the Wild is in development by Nintendo. The sequel is part of The Legend of Zelda series and is currently set to be released in 2022 for the Nintendo Switch",
       "PRO":"31",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Nintendo"
    },
    {
       "CAL":"Pokemon Legends: Arceus",
@@ -151,7 +151,7 @@
       "FBR":"90",
       "ITEM":"Pokémon Legends: Arceus is an upcoming 2022 action role-playing game developed by Game Freak and published by Nintendo and The Pokémon Company for the Nintendo Switch",
       "PRO":"35",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Nintendo"
    },
    {
       "CAL":"Super Mario 3D All-Stars",
@@ -162,6 +162,6 @@
       "FBR":"0",
       "ITEM":"Super Mario 3D All-Stars is a 2020 compilation of platform games for the Nintendo Switch. It commemorates the 35th anniversary of Nintendos Super Mario franchise, with high-definition ports of Super Mario 64, Super Mario Sunshine, and Super Mario Galaxy",
       "PRO":"27",
-      "CATEGORY":"BURGERSANDWICH"
+      "CATEGORY":"Nintendo"
    }
 ]
